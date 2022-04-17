b6bba2f952e558ce65c3b4ab7fb5bdffabddf4c1
KoHtet <tonybright008@gmail.com>
2022-04-17 06:28:44 -0700

    Last stage of the website

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const express = require('express')
-
-// App Setup 
+const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
 
-// Server Setup
-const server = app.listen(3000, () => {
-    console.log('Server started successfully at port 4000')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+io.on('connection', (socket) => {
+    console.log('A user Connected' + socket.id)
+    socket.on('disconnect', () => {
+        console.log('User disconnected' + socket.id)
+    })
+    socket.on('Chat meassage', (msg) => {
+        io.sockets.emit('Chat message', msg);
+    })
+    socket.on('sender', function (data) {
+        socket.broadcast.emit('sender', data);
+        console.log("Event exist")
+    })
 })
 
-// Route Setup
-app.get('/', (req, rep) => {
-    rep.sendFile(__dirname + '/public/index.html');
-})
+server.listen(3000, () => {
+    console.log('Listening on *:3000')
+});
